2ccc5c4838491515af429fa19fccae857a564b82
Aren <aren.dolbakian@gmail.com>
2022-06-14 04:09:01 -0400

    Kata 4 - Instructors Names

--- katas/longest_name.js
+++ katas/longest_name.js
@@ -1,12 +1,11 @@
-const instructorWithLongestName = function(instructors) {
+const instructorWithLongestName = function (instructors) {
   let indexOfLongest = 0;
-  for (i = 0; i < instructors.length; i++){
+  for (i = 0; i < instructors.length; i++) {
     if (instructors[i]['name'].length > instructors[indexOfLongest]['name'].length) {
-     indexOfLongest = i;
-    } 
+      indexOfLongest = i;
+    }
   }
   return instructors[indexOfLongest];
-
 };
 
 
@@ -14,13 +13,13 @@ const instructorWithLongestName = function(instructors) {
 
 
 console.log(instructorWithLongestName([
-  {name: "Samuel", course: "iOS"},
-  {name: "Jeremiah", course: "Web"},
-  {name: "Ophilia", course: "Web"},
-  {name: "Donald", course: "Web"}
+  { name: "Samuel", course: "iOS" },
+  { name: "Jeremiah", course: "Web" },
+  { name: "Ophilia", course: "Web" },
+  { name: "Donald", course: "Web" }
 ]));
 console.log(instructorWithLongestName([
-  {name: "Matthew", course: "Web"},
-  {name: "David", course: "iOS"},
-  {name: "Domascus", course: "Web"}
+  { name: "Matthew", course: "Web" },
+  { name: "David", course: "iOS" },
+  { name: "Domascus", course: "Web" }
 ]));
